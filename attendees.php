9edf3d1df1515c5a57a61f1baac462ec34f60b13
Saurabh Shukla <saurabh@yapapaya.com>
2015-07-13 19:02:17 +0530

    Added [#5] as a count parameter in get fixed attendees list

--- attendees.php
+++ attendees.php
@@ -7,20 +7,56 @@ global $wpdb;
 
 $tbl = $wpdb->prefix . 'wcp_tickets';
 
-$waste_coupons = array(
-        'WCP1557FEM',
-        'FREE',
-        'WCP15SPKRSAU',
-        'WCP15FEM020',
-        'WCP15SIR020',
-        'WCP15VIP040',
-        'WCPFREE9SUCVSFQ8'
+$counts = $wpdb->get_results( "SELECT `role`, COUNT(*) as count FROM $tbl GROUP BY `role` LIMIT 5" );
+
+$roles = array(
+        'Writer/ Blogger',
+        'Developer',
+        'Site Admin',
+        'Designer',
+        'Manager',
+        'Other'
 );
 
-$waste_coupon_query_str = implode( "', '", $waste_coupons );
+function array_orderby() {
+    $args = func_get_args();
+    $data = array_shift( $args );
+    foreach ( $args as $n => $field ) {
+        if ( is_string( $field ) ) {
+            $tmp = array();
+            foreach ( $data as $key => $row )
+                $tmp[ $key ] = $row[ $field ];
+            $args[ $n ] = $tmp;
+        }
+    }
+    $args[] = &$data;
+    call_user_func_array( 'array_multisort', $args );
+    return array_pop( $args );
+}
 
-$attendees = $wpdb->get_results( "SELECT * from $tbl where coupon NOT IN ('" . $waste_coupon_query_str . "') ORDER BY ticket_id DESC" );
+$counts = json_decode( json_encode( $counts ), true );
+$sortcount = $counts;
+$sortcount = array_orderby( $counts, 'count', SORT_DESC, 'role', SORT_ASC );
+
+$full = $sortcount[ 0 ][ 'count' ];
+$barcount = count( $counts );
+$incr = 0;
 ?>
+<ul id="wcp-role-distro">
+    <?php
+    foreach ( $sortcount as $role ) {
+        if ( in_array( $role[ 'role' ], $roles ) ) {
+
+            $role_img = explode( '/', $role[ 'role' ] );
+            $role_img = strtolower( trim( $role_img[ 0 ] ) );
+            ?>
+            <li style="height:<?php echo round( ($role[ 'count' ] / $full * 100) * 3 / 4, 2 ); ?>%;left:<?php echo round( $incr / $barcount * 100 ); ?>%;" title="<?php echo $role[ 'role' ] . '(s)'; ?>"><span><img src="https://pune.wordcamp.org/2015/files/2015/07/<?php echo strtolower( $role_img ); ?>.png" alt="<?php echo $role[ 'role' ] . '(s)'; ?>" width="24" height="24" /><sup><?php echo $role[ 'count' ]; ?></sup></span></li>
+            <?php
+            $incr ++;
+        }
+    }
+    ?>
+</ul>
 <ul id="wcp-attendees-legend">
     <li><i class="icon-twitter"></i> Twitter</li>
     <li><i class="icon-link"></i> Website</li>
@@ -30,59 +66,60 @@ $attendees = $wpdb->get_results( "SELECT * from $tbl where coupon NOT IN ('" . $
     <li><i class="icon-glass"></i> After-party</li>
 </ul>
 <div id="wcp-attendees">
-
     <?php
+    $attendees = $wpdb->get_results( "SELECT * from $tbl ORDER BY name ASC" );
+ 
     $count = 0;
-    foreach ( $attendees as $attendee ) {
-        $count++;
-        $twitter = stripslashes( preg_replace( '/^((http(s)*:\/\/)*twitter.com\/)*(@)*/i', '', $attendee->twitter ) );
-        $party = boolval( $attendee->party );
-        switch ( strtoupper( $attendee->type ) ) {
-            case 'ORGR':
-                $icon = 'group';
-                $alt = 'Organiser';
-                break;
-            case 'SPKR':
-                $icon = 'microphone';
-                $alt = 'Speaker';
-                break;
-            case 'VOLR':
-                $icon = 'hand-up';
-                $alt = 'Volunteer';
-                break;
-            default:
-                $icon = '';
-                break;
-        }
-        if ( ! empty( $attendee->name ) ) {
-            ?>
-            <div class="wcp-attendee">
-                <?php echo get_avatar( $attendee->email, 196, 'wavatar', ucwords( $attendee->name ) ); ?>
-                <div class="wcp-attendee-details">
-                    <h2><?php echo ucwords( $attendee->name ); ?></h2>
-                    <p><?php echo ucwords( $attendee->role ); ?></p>
-                    <ul>
-                        <?php if ( ! empty( $twitter ) ) { ?>
-                            <li class="wcp-twitter wcp-link"><a href="https://twitter.com/<?php echo $twitter; ?>" title="@<?php echo $twitter; ?>"><i class="icon-twitter"></i></a></li>
-                        <?php } ?>
-                        <?php if ( ! empty( $attendee->website ) ) { ?>
-                            <li class="wcp-website wcp-link"><a href="<?php echo $attendee->website; ?>" title="<?php echo $attendee->website; ?>"><i class="icon-link"></i></a></li>
-                        <?php } ?>
-                        <?php if ( ! empty( $icon ) ) { ?>
-                            <li class="wcp-type"><i class="icon-<?php echo $icon; ?> non-link" title="<?php echo $alt; ?>"></i></li>
-                        <?php } ?>
-                        <?php if ( $party ) { ?>
-                            <li class="wcp-party"><i class="icon-glass non-link" title="Coming to the after-party"></i></li>
-                            <?php } ?>
+    
+        foreach ( $attendees as $attendee ) {
 
-                    </ul>
+            $count ++;
+            $twitter = stripslashes( preg_replace( '/^((http(s)*:\/\/)*twitter.com\/)*(@)*/i', '', $attendee->twitter ) );
+            $party = boolval( $attendee->party );
+            switch ( strtoupper( $attendee->type ) ) {
+                case 'ORGR':
+                    $icon = 'group';
+                    $alt = 'Organiser';
+                    break;
+                case 'SPKR':
+                    $icon = 'microphone';
+                    $alt = 'Speaker';
+                    break;
+                case 'VOLR':
+                    $icon = 'hand-up';
+                    $alt = 'Volunteer';
+                    break;
+                default:
+                    $icon = '';
+                    break;
+            }
+            if ( ! empty( $attendee->name ) ) {
+                ?>
+                <div class="wcp-attendee">
+                    <?php echo get_avatar( $attendee->email, 196, 'wavatar', ucwords( $attendee->name ) ); ?>
+                    <div class="wcp-attendee-details">
+                        <h2><?php echo ucwords( $attendee->name ); ?></h2>
+                        <p><?php echo ucwords( $attendee->role ); ?></p>
+                        <ul>
+                            <?php if ( ! empty( $twitter ) ) { ?>
+                                <li class="wcp-twitter wcp-link"><a href="https://twitter.com/<?php echo $twitter; ?>" title="@<?php echo $twitter; ?>"><i class="icon-twitter"></i></a></li>
+                            <?php } ?>
+                            <?php if ( ! empty( $attendee->website ) ) { ?>
+                                <li class="wcp-website wcp-link"><a href="<?php echo $attendee->website; ?>" title="<?php echo $attendee->website; ?>"><i class="icon-link"></i></a></li>
+                            <?php } ?>
+                            <?php if ( ! empty( $icon ) ) { ?>
+                                <li class="wcp-type"><i class="icon-<?php echo $icon; ?> non-link" title="<?php echo $alt; ?>"></i></li>
+                            <?php } ?>
+                            <?php if ( $party ) { ?>
+                                <li class="wcp-party"><i class="icon-glass non-link" title="Coming to the after-party"></i></li>
+                                <?php } ?>
+
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <?php
+                <?php
+            }
         }
-        if($count>59 && $count%60===1){
-            echo '<!--nextpage-->';
-        }
-    }
-    ?>
-</div>
+        ?>
+    </div>
+<div class="wcp-note" style="clear:both;width:100%"><p>Icons used in the bar graph on top are made by <a href="http://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> and are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></p></div>
--- student-code-generator.php
+++ student-code-generator.php
@@ -1,9 +1,12 @@
 <?php
+
 /*
  * Template name: Student code generator
  */
 $prefix = 'WCP15STUD';
-$random = wp_generate_password(6, false);
-$code = $prefix.$random;
-echo $code;
-
+$count = isset($_GET[ 'count' ])? intval($_GET['count']):1;
+for ( $i = 0; $i < $count; $i ++  ) {
+    $random = wp_generate_password( 6, false );
+    $code = $prefix . $random;
+    echo $code . "<br>";
+}
